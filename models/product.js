571e66af266f8db2181ee54f6087e498ded524a0
deepak <d1612k@gmail.com>
2019-03-12 00:16:08 +0530

    minor changes

--- models/product.js
+++ models/product.js
@@ -26,6 +26,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = (Math.random()*1000).toString() 
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
